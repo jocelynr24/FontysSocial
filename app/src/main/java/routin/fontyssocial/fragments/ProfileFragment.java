f33fbce4125c46017d697b776239e7b4171b0488
Jocelyn <jocelyn.routin@outlook.fr>
2018-01-19 15:04:36 +0100

    Profile page works and localization
    + Linked the profile page to the database (read/write)
    + Added some strings in localization

--- app/src/main/java/routin/fontyssocial/fragments/ProfileFragment.java
+++ app/src/main/java/routin/fontyssocial/fragments/ProfileFragment.java
@@ -14,10 +14,22 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Locale;
+import java.util.Map;
 
 import routin.fontyssocial.R;
+import routin.fontyssocial.SignUpActivity;
+import routin.fontyssocial.main.MainActivity;
+import routin.fontyssocial.model.User;
 
 public class ProfileFragment extends Fragment {
     private View myView;
@@ -27,6 +39,14 @@ public class ProfileFragment extends Fragment {
     private EditText text_lastname;
     private RadioButton rb_shareyes;
     private RadioButton rb_shareno;
+
+    private String firstname;
+    private String lastname;
+    private boolean sharepos;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference users = database.getReference("users");
 
     @Nullable
     @Override
@@ -38,7 +58,8 @@ public class ProfileFragment extends Fragment {
         rb_shareyes = (RadioButton) myView.findViewById(R.id.rb_shareyes);
         rb_shareno = (RadioButton) myView.findViewById(R.id.rb_shareno);
 
-        fillTheFields(); // to be coded
+        // Fill the fields according to the data
+        fillTheFields();
 
         this.arraySpinner = new String[] {
                 myView.getContext().getString(R.string.profile_en), myView.getContext().getString(R.string.profile_fr)
@@ -53,12 +74,25 @@ public class ProfileFragment extends Fragment {
             public void onClick(View v) {
                 // When user apply changes
 
-                // Profile part
-                // todo
+                // Profile part (get the fields)
+                firstname = text_firstname.getText().toString();
+                lastname = text_lastname.getText().toString();
+                if(rb_shareyes.isChecked()){
+                    sharepos = true;
+                } else {
+                    sharepos = false;
+                }
+
+                // Profile part (send to the database)
+                users.child(User.getInstance().getName()).child("settings").child("firstname").setValue(firstname);
+                users.child(User.getInstance().getName()).child("settings").child("lastname").setValue(lastname);
+                users.child(User.getInstance().getName()).child("settings").child("share").setValue(sharepos);
 
                 // Language part
                 spinner_lang = mySpinner.getSelectedItem().toString();
                 changeLanguage(getResources(), spinner_lang);
+
+                Toast.makeText(getContext(), getString(R.string.settings_saved), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,8 +100,10 @@ public class ProfileFragment extends Fragment {
         fab_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // When user cancel changes
-                fillTheFields(); // to be coded
+                MainActivity ma = ((MainActivity) getActivity());
+                ma.getFragmentManager().beginTransaction().replace(R.id.content_frame, ma.mapEventFragment).commit();
+
+                Toast.makeText(getContext(), getString(R.string.settings_cancelled), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -103,10 +139,44 @@ public class ProfileFragment extends Fragment {
     }
 
     private void fillTheFields(){
-        // todo: fill the options fields at startup or when cancelled, must be coded
-        text_firstname.setText("Firstname");
-        text_lastname.setText("Lastname");
-        rb_shareyes.setChecked(true);
-        rb_shareno.setChecked(false);
+        users.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists()){
+                            Map<String, Object> users = (Map<String, Object>) dataSnapshot.getValue();
+
+                            for (Map.Entry<String, Object> entry : users.entrySet()) {
+
+                                String name = entry.toString().split("=")[0];
+
+                                if (name.equals(User.getInstance().getName())) {
+                                    Map user = (Map) entry.getValue();
+
+                                    firstname = (String) ((Map) user.get("settings")).get("firstname");
+                                    lastname = (String) ((Map) user.get("settings")).get("lastname");
+                                    sharepos = (boolean) ((Map) user.get("settings")).get("share");
+
+                                    text_firstname.setText(firstname);
+                                    text_lastname.setText(lastname);
+                                    if(sharepos){
+                                        rb_shareyes.setChecked(true);
+                                        rb_shareno.setChecked(false);
+                                    }
+                                    if(!sharepos){
+                                        rb_shareyes.setChecked(false);
+                                        rb_shareno.setChecked(true);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
     }
 }
